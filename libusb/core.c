f621dd8a2eb2f9ada43320118b2f91088f22ca4b
Georges-Etienne Legendre <legege@legege.com>
2013-03-10 15:34:39 -0400

    removing date from log

--- libusb/core.c
+++ libusb/core.c
@@ -1753,61 +1753,46 @@ int usbi_gettimeofday(struct timeval *tp, void *tzp)
 void usbi_log_v(struct libusb_context *ctx, enum usbi_log_level level,
 	const char *function, const char *format, va_list args)
 {
-	FILE *stream = stdout;
-	const char *prefix;
-	struct timeval now;
-	static struct timeval first = { 0, 0 };
+  FILE *stream = stdout;
+  const char *prefix;
 
 #ifndef ENABLE_DEBUG_LOGGING
-	USBI_GET_CONTEXT(ctx);
-	if (!ctx->debug)
-		return;
-	if (level == LOG_LEVEL_WARNING && ctx->debug < 2)
-		return;
-	if (level == LOG_LEVEL_INFO && ctx->debug < 3)
-		return;
+  USBI_GET_CONTEXT(ctx);
+  if (!ctx->debug)
+    return;
+  if (level == LOG_LEVEL_WARNING && ctx->debug < 2)
+    return;
+  if (level == LOG_LEVEL_INFO && ctx->debug < 3)
+    return;
 #endif
 
-	usbi_gettimeofday(&now, NULL);
-	if (!first.tv_sec) {
-		first.tv_sec = now.tv_sec;
-		first.tv_usec = now.tv_usec;
-	}
-	if (now.tv_usec < first.tv_usec) {
-		now.tv_sec--;
-		now.tv_usec += 1000000;
-	}
-	now.tv_sec -= first.tv_sec;
-	now.tv_usec -= first.tv_usec;
+  switch (level) {
+  case LOG_LEVEL_INFO:
+    prefix = "info";
+    break;
+  case LOG_LEVEL_WARNING:
+    stream = stderr;
+    prefix = "warning";
+    break;
+  case LOG_LEVEL_ERROR:
+    stream = stderr;
+    prefix = "error";
+    break;
+  case LOG_LEVEL_DEBUG:
+    stream = stderr;
+    prefix = "debug";
+    break;
+  default:
+    stream = stderr;
+    prefix = "unknown";
+    break;
+  }
 
-	switch (level) {
-	case LOG_LEVEL_INFO:
-		prefix = "info";
-		break;
-	case LOG_LEVEL_WARNING:
-		stream = stderr;
-		prefix = "warning";
-		break;
-	case LOG_LEVEL_ERROR:
-		stream = stderr;
-		prefix = "error";
-		break;
-	case LOG_LEVEL_DEBUG:
-		stream = stderr;
-		prefix = "debug";
-		break;
-	default:
-		stream = stderr;
-		prefix = "unknown";
-		break;
-	}
+  fprintf(stream, "libusb:%s [%s] ", prefix, function);
 
-	fprintf(stream, "libusb: %d.%06d %s [%s] ",
-		(int)now.tv_sec, (int)now.tv_usec, prefix, function);
+  vfprintf(stream, format, args);
 
-	vfprintf(stream, format, args);
-
-	fprintf(stream, "\n");
+  fprintf(stream, "\n");
 }
 
 void usbi_log(struct libusb_context *ctx, enum usbi_log_level level,
